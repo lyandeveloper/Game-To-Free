7e754c3ef3a39be5e0cad9996985644cbe97b7c3
lyandeveloper <tutors3d@gmail.com>
2022-08-22 14:23:39 -0300

    feat: style hero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { SwiperModule } from 'swiper/angular';
+import { GameCardComponent } from './game-card/game-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BannerComponent,
+    GameCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SwiperModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
