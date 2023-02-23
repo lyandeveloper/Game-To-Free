56eb80fbcdf116fc12a0ea7b06a4796d33669ae6
lyandeveloper <tutors3d@gmail.com>
2023-02-23 08:25:48 -0300

    chore: comment unused code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { GameService } from '../services/game-service/game.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  games: any = [];
+  page = 1;
 
-  games: any = []
-
-  constructor(private gameService: GameService) { }
+  constructor(
+    private gameService: GameService,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
-    this.getGames()
+    this.getGames();
+  }
+
+  loadMore() {
+    // this.page.subscribe()
   }
 
   getGames() {
     this.gameService.getGames().subscribe((games: any) => {
-      this.games = this.gameService.paginate(games, 1, 8);
-      this.gameService.parseCategories(this.games)
+      this.games = this.gameService.paginate(games, this.page, 8);
+      this.gameService.parseCategories(this.games);
     });
   }
-
 }
